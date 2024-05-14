ee2674d297f52f63223354a47c4831d4ba542228
Sofia Rodriguez del Busto <rdbsofi@gmail.com>
2024-05-14 12:25:02 -0300

    Ejercicio 1 resuelto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+int a;
+int b;
+a = 10;
+b = a;
+Console.WriteLine("El valor de a es: "+a);
+Console.WriteLine("El valor de b es: "+b);
+
+Console.WriteLine("\n--------Ejercicio 1--------\n");
+Console.WriteLine("Ingrese un numero");
+string cadena = Console.ReadLine(); 
+int num;
+int resto;
+int numInvertido = 0;
+if(int.TryParse(cadena, out num))
+{
+   if(num > 0)
+   {
+      while(num != 0)
+      {
+        resto = num%10;
+        num = num/10;
+        numInvertido = numInvertido*10 + resto;
+      }
+   }
+   Console.WriteLine($"El numero invertido es: {numInvertido}");
+}
+Console.WriteLine("No ha ingresado un numero");
